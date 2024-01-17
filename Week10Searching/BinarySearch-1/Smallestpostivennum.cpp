fa9d57a5358b1761e8a3745b5ce26f938a71bd1e
vishal1202verma <vishalvermao.pcm121@gmail.com>
2024-01-17 19:09:14 +0530

    commited

---
+++ Week10Searching/BinarySearch-1/Smallestpostivennum.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main (){
+int arr[9] = {0,1,2,3,4,5,8,7};
+int n  = 9;
+// int target = 3;
+// linear serach
+for(int i=0; i<n; i++){
+    if( i != arr[i]){
+        cout<<"this is the minimum sorted number in the array : "<<i<<endl;
+        break;
+    }
+}
+
+// binary search
+cout<<"vishal verma ji ";
+
+
+
+
+
+
+
+
+}
