9c636162b4acde77b3f3446ed53a346c64168dd5
vishal1202verma <vishalvermao.pcm121@gmail.com>
2024-01-18 12:29:06 +0530

    binary search lecture one commited

--- Week10Searching/BinarySearch-1/Smallestpostivennum.cpp
+++ Week10Searching/BinarySearch-1/Smallestpostivennum.cpp
@@ -1,24 +1,32 @@
 #include<iostream>
 using namespace std;
 int main (){
-int arr[9] = {0,1,2,3,4,5,8,7};
+int arr[9] = {0,1,2,3,4,5,7,8};
 int n  = 9;
 // int target = 3;
 // linear serach
-for(int i=0; i<n; i++){
-    if( i != arr[i]){
-        cout<<"this is the minimum sorted number in the array : "<<i<<endl;
-        break;
-    }
-}
+// for(int i=0; i<n; i++){
+//     if( i != arr[i]){
+//         cout<<"this is the minimum sorted number in the array : "<<i<<endl;
+//         break;
+//     }
+// }
 
 // binary search
-cout<<"vishal verma ji ";
-
-
-
-
-
+int lo =0;
+int hi =n-1;
+int ans  = -1;
+while(lo<=hi){
+int mid = lo + (hi-lo)/2;
+   if(arr[mid] == mid ){
+           lo = mid +1;
+     }
+     else{
+        ans =mid;
+        hi = mid -1;
+     }
+}
+cout<<"this is the minimum mising number of this sorted array : "<<ans;
 
 
 
