2e0ddc254cd5a73226b25f4b1ea0ab48ee7c6c6f
vishal1202verma <vishalvermao.pcm121@gmail.com>
2024-02-29 14:16:18 +0530

    BItwiseOperator other half lecture commited

--- Week13AdvanceMathematics/BitwiseOperator/FindDuplicateNum.cpp
+++ Week13AdvanceMathematics/BitwiseOperator/FindDuplicateNum.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main (){
-int arr[]  = {2,2,4,1,5,1,4,5,6};
+int arr[]  = {2,2,4,1,5,1,4,5,6, 6 ,10};
 int n = sizeof(arr)/sizeof(arr[0]);
 int ans = 0;
 for(int i=0; i<n; i++){ // T.C. =  O(n);
@@ -10,4 +10,7 @@ for(int i=0; i<n; i++){ // T.C. =  O(n);
 cout<<ans;
 return 0;
 
-}
+
+
+
+ }
---
+++ Week13AdvanceMathematics/BitwiseOperator/tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+  res = retvel ^res;
+  cout<<res<<" ";
