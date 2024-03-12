da94e1baf8db819f9829ecd61821a208a2c658e2
vishal1202verma <vishalvermao.pcm121@gmail.com>
2024-03-12 12:30:10 +0530

    Leetcode classAndobjects is commited

---
+++ Week14_Oops/Oops-3/ClassesAndObjects.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+using namespace std;
+class player{
+    private :
+    int health;
+    int age;
+    int score;
+    bool alive;
+
+    public :
+    int getHealth(){
+        return health;
+    }
+    int getAge(){
+        return age;
+    }
+    int getScore(){
+        return score;
+    }
+    bool isAlive(){
+        return alive;
+    }
+    void setHealth(int health){
+          this->health = health;
+    }
+    void setAge( int age){
+          this->age = age;
+    }
+    void setScore(int score){
+          this->score = score;
+    }
+    void setisalive( bool alive){
+          this->alive = alive;
+    }
+};
+
+int addScore(player a, player b){
+       return a.getScore() + b.getScore();
+}
+
+// return type is the class's object of the function
+player getMAxScore(player a, player b){
+       if(a.getScore()> b.getScore()){
+              return a;
+       } 
+       else{
+        return b;
+       }
+
+
+}
+int main (){
+//  making a object
+// staticly object creation
+player vishal; // compile , static memry allocation
+vishal.setHealth(100);
+vishal.setAge(22);
+vishal.setScore(264);
+vishal.setisalive(true);
+
+player Ragav; // compile , static memry allocation
+Ragav.setHealth(99);
+Ragav.setAge(28);
+Ragav.setScore(100);
+Ragav.setisalive(true);
+
+cout<<addScore(vishal, Ragav )<<endl;
+player sanket = getMAxScore(vishal , Ragav );
+cout<<sanket.getScore()<<endl;
+
+// Dynamic memory allocations
+
+player *shree = new player; // run time , dynamic memory allocation
+shree->setScore(100);
+shree->setHealth(101);
+shree->setAge(20);
+
+cout<<shree->getScore();
+
+
+
+
+
+
+
+}
--- Week14_Oops/Oops-3/demo.cpp
+++ Week14_Oops/Oops-3/demo.cpp
@@ -5,8 +5,7 @@ class player{
      int ID;
      int score;
      string name;
-
-
+     
 };
 
 int main (){
@@ -15,6 +14,15 @@ playerData.ID = 45;
 playerData.score = 100;
 playerData.name = "Rohit Sharma";
 cout<<playerData.name;
+player x;
+player y;
+x.ID =400;
+x.score = 500;
+y.ID = 400;
+y.score = 500;
+
+
+
 
 
 
