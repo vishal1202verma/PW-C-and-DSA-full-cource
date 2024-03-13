f654aa8c9bbbcf7930b09bc55e6eb38a3bca7337
vishal1202verma <vishalvermao.pcm121@gmail.com>
2024-03-13 23:43:44 +0530

    Commited

---
+++ Week14_Oops/Oops-3/nestedClass.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class player{
+    public :
+    int health;
+    int age;
+    int score;
+    bool alive;
+
+
+      class Gun{
+      public :
+        int ammo;
+        int damage;
+        int scope;
+      };
+    
+
+};
+int main (){
+
+
+
+    
+}
