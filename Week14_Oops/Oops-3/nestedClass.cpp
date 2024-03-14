93ecc66447fe8e21a57f48187e1c82fa8e0f0cde
vishal1202verma <vishalvermao.pcm121@gmail.com>
2024-03-14 23:53:36 +0530

    Nested class is commited

--- Week14_Oops/Oops-3/nestedClass.cpp
+++ Week14_Oops/Oops-3/nestedClass.cpp
@@ -8,19 +8,21 @@ class player{
     int score;
     bool alive;
 
-
       class Gun{
       public :
         int ammo;
         int damage;
         int scope;
       };
-    
-
+  
 };
 int main (){
+player vishal;
+vishal.age = 50;
+vishal.alive = true;
+vishal.health = 100;
+vishal.score = 264;
 
-
-
-    
+cout<<vishal.score;
+   
 }
