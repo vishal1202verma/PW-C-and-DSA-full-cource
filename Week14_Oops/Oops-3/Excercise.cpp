aac00663f30f9a26e317cb2147465dd332b4c8f4
vishal1202verma <vishalvermao.pcm121@gmail.com>
2024-03-15 12:11:48 +0530

    Opps -3 Excercise question lectures is commited

--- Week14_Oops/Oops-3/Excercise.cpp
+++ Week14_Oops/Oops-3/Excercise.cpp
@@ -1,2 +1,57 @@
 #include<iostream>
-         
+using namespace std;
+
+class cricketer {
+   private :
+   string name;
+   int age;
+   int NotestMatches;
+   int AvgScore;
+
+   public :
+   string getNameofGreatplayer( ){
+        return  name; 
+   }
+
+   int getAge(){
+    return age;
+   }
+   
+   int getNotestMatches(){
+    return  NotestMatches;
+
+   }
+   int getAvgScores(){
+     return AvgScore;
+   }
+   void setName( string name){
+        this->name = name;
+
+   }
+
+   void setAge( int age){
+      this->age = age;
+   }
+
+   void setNotestMatches( int NotestMatches){
+     this->NotestMatches = NotestMatches;
+   }
+   
+   void setAvgScore( int AvgScore){
+         this->AvgScore = AvgScore;
+   } 
+};
+
+int main (){
+cricketer indianTeam;
+indianTeam.setName( "Rohit Sharma");
+indianTeam.setAge(35);
+indianTeam.setNotestMatches(100);
+indianTeam.setAvgScore(60);
+
+
+cout<<indianTeam.getNameofGreatplayer();
+
+
+
+}
