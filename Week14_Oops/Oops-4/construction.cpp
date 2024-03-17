3b6de7b6704ae971b559169f01f937b5c05dcb29
vishal1202verma <vishalvermao.pcm121@gmail.com>
2024-03-17 12:36:54 +0530

    Oops-4 constructor is commited

---
+++ Week14_Oops/Oops-4/construction.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<climits>
+using namespace std;
+class Bike{
+   public :
+     int tyreSize; // datatyeps 
+     int engineSize; // datatypes
+
+    Bike( int tyreSize, int engineSize){ // constructor creation 
+    // values initilized
+    this->tyreSize = tyreSize;
+    this->engineSize = engineSize;
+    cout<<"constructor is called"<<endl;
+
+    }         
+
+
+};
+
+int main (){
+Bike TVS( 5, 125);  // object creation // cunstructor call it is a hidden function // by default in compiler
+Bike honda(3, 150);
+Bike bajaj(8, 220);
+Bike sujuki(9, 250);
+cout<<TVS.tyreSize<<" "<<TVS.engineSize<<endl;
+cout<<honda.tyreSize<<" "<<honda.engineSize<<endl;
+cout<<sujuki.tyreSize<<" "<<bajaj.engineSize <<endl;
+cout<<bajaj.tyreSize<<" "<<sujuki.engineSize<<endl;
+    
+
+
+
+
+
+}
