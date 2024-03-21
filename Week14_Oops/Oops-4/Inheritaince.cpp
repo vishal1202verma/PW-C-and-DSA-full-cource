58a8607b6281580221417120af63d5825c196c78
vishal1202verma <vishalvermao.pcm121@gmail.com>
2024-03-21 20:18:22 +0530

    Comitted

--- Week14_Oops/Oops-4/Inheritaince.cpp
+++ Week14_Oops/Oops-4/Inheritaince.cpp
@@ -2,17 +2,28 @@
 #include<algorithm>
 #include<climits>
 using namespace std;
-
 class vehical{
     public :
     int tyreSize;
     int engineSize;
     int lights;
     string companyName;
+    // fuction member of the class 
+    void showCompany(){
+       cout<<"show the company name";
+                                                          
+              
+    }
+
+
+    // vehical constructor 
+    vehical (){
+        cout<<"Vehical constructor call huna hain"<<endl;
+    }
          
 
 };
-// Inhertaince of vehical class OR child of vehical
+// Inheritance of vehical class OR child of vehical
 class Car : public vehical {    
     public :
     int steeringSize;
@@ -22,6 +33,22 @@ class Car : public vehical {
 class Bike : public vehical{
   public :
     int handleSize;
+    // constructor of bike class 
+    Bike(){
+        cout<<"bike ka constructor call ho raha hain"<<endl;
+    }
+
+};
+
+class A{
+    private :
+               
+
+};
+
+class B{
+   private :
+
 
 };
 
@@ -34,5 +61,6 @@ int main (){
     cout<<honda.companyName<<endl;
     cout<<honda.handleSize<<endl;
     cout<<honda.lights<<endl;
-
+    honda.showCompany();
+     
 }
