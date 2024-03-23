35476daf33cdb17d77f5df90dc98efa94ddd53fb
vishal1202verma <vishalvermao.pcm121@gmail.com>
2024-03-23 19:36:32 +0530

    aryan verma commited

--- Week14_Oops/Oops-4/Streak.cpp
+++ Week14_Oops/Oops-4/Streak.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
 using namespace std;
 int main (){
-  cout<<"This is streak sequnce only";
-   
+  cout<<"This is streak sequnce only"<<endl;
+  cout<<" aryan verma is a good boy "<<endl;
+  cout<<"vishal is a good boy";
+  cout<<"congratulation ! your code is working  ";  
+
 
 }
