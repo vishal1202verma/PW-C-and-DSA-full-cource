3517469be6251bcd985804c2735c0989d0279d9e
vishal1202verma <vishalvermao.pcm121@gmail.com>
2024-03-24 17:52:05 +0530

    Oops-4 commited

--- Week14_Oops/Oops-4/Inheritaince.cpp
+++ Week14_Oops/Oops-4/Inheritaince.cpp
@@ -11,17 +11,14 @@ class vehical{
     // fuction member of the class 
     void showCompany(){
        cout<<"show the company name";
-                                                          
-              
+                                                            
     }
-
 
     // vehical constructor 
     vehical (){
         cout<<"Vehical constructor call huna hain"<<endl;
     }
          
-
 };
 // Inheritance of vehical class OR child of vehical
 class Car : public vehical {    
@@ -37,30 +34,49 @@ class Bike : public vehical{
     Bike(){
         cout<<"bike ka constructor call ho raha hain"<<endl;
     }
-
+    
 };
 
 class A{
-    private :
-               
+    private : // private can not be acced and canot be inherited
+    int a_ka_private;
+
+    protected: // protected canot be acced bu can inherited
+    int a_ka_protected;
+
+    public : // can be acced and inherited
+     int a_ka_public;              
 
 };
 
-class B{
-   private :
+class B : public A { // B is the child of the A class
+   public :
+   int b_ka_public;
 
+//    protected :
+//    int b_ka_protected;
+
+//    private :
+//    int b_ka_private;
 
 };
+
 
 int main (){
     Bike honda;
+    A a;
+    B b;
     honda.handleSize = 5;
     honda.engineSize = 220;
     honda.lights = 500;
     honda.companyName = "Splender";
+    honda.tyreSize = 230;
+    
     cout<<honda.companyName<<endl;
     cout<<honda.handleSize<<endl;
     cout<<honda.lights<<endl;
     honda.showCompany();
-     
-}
+    cout<<endl;
+    cout<<honda.tyreSize<<endl;   
+      
+}    
