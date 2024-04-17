7521922052ce27bd6ad808ddba9dad4cbb61ce4d
vishal1202verma <vishalvermao.pcm121@gmail.com>
2024-04-17 13:37:31 +0530

    display rec is commited

--- Week16_Stack/Stack-1/revdisplayRec.cpp
+++ Week16_Stack/Stack-1/revdisplayRec.cpp
@@ -5,10 +5,10 @@ using namespace std;
 // print the recursive stack 
 void  displayRec( stack<int> &st){
     if( st.size() == 0) return;
+     cout<<st.top()<<" ";
      int x = st.top();
      st.pop();
      displayRec(st);
-     cout<<x<<" ";
      st.push(x);
 
 }
